8d45ffaa4618c309d3748e4ddf8ba6e5ac0e7caa
coffeeandice <625740749@qq.com>
2019-04-09 16:53:46 +0800

    脱敏处理

--- src/main/java/ChatApplication.java
+++ src/main/java/ChatApplication.java
@@ -1,15 +1,13 @@
-import initApplication.InitChatApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"initApplication","dataHandle","channelHandle"})
 @SpringBootApplication
-public class AqishuChatApplication {
+public class ChatApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AqishuChatApplication.class, args);
+        SpringApplication.run(ChatApplication.class, args);
     }
 
 
